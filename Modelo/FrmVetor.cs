4370291f256109ba15398f0a5ef67d315aef0100
Matheus Franco <matheusefranco@gmail.com>
2021-10-01 07:58:02 -0300

    Start

---
+++ Modelo/FrmVetor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modelo
+{
+    public partial class FrmVetor : Form
+    {
+        
+
+        public FrmVetor()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FrmVetor_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnOrdenar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void BtnBuscaBinaria_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCarregar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSalvar_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void listCidades_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void mostrar()
+        {
+         
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
